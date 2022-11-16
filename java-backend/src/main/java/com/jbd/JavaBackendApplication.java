8292de68804f2fb578bd424f8424d149969213f0
Digvijay Jatkar <jatkardigvijay@gmail.com>
2022-11-16 15:16:13 +0000

    Project structure created test file added and checked

---
+++ java-backend/src/main/java/com/jbd/JavaBackendApplication.java
@@ -0,0 +1,16 @@
+package com.jbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendApplication.class, args);
+		
+		System.out.println("Hello World");
+		System.out.println("Test Commit");
+	}
+
+}
