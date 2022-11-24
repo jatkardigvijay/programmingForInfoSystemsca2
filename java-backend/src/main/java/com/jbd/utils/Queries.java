ef05e330375fb185c4114a8a0b14580bd87d9e4a
Digvijay Jatkar <jatkardigvijay@gmail.com>
2022-11-24 00:45:56 +0000

    code restructure and sql documentation added

--- java-backend/src/main/java/com/jbd/utils/Queries.java
+++ java-backend/src/main/java/com/jbd/utils/Queries.java
@@ -3,8 +3,8 @@ package com.jbd.utils;
 public class Queries {
 
 	public static final String GET_ALL_STUDENTS = "select * from student order by studentId";
-	public static final String GET_STUDENT_BY_ID = "select * from student where id = ?";
-	public static final String DELETE_STUDENT_BY_ID = "delete from student where id = ?";
+	public static final String GET_STUDENT_BY_ID = "select * from student where studentId = ?";
+	public static final String DELETE_STUDENT_BY_ID = "delete from student where studentId = ?";
 	public static final String INSERT_STUDENT = "insert into student values (?,?,?)";
 	public static final String UPDATE_STUDENT = "update student set firstName = ?, lastName = ?, email = ? where id = ?";
 }
