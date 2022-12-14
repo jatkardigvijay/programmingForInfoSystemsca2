bc36a9cf78f5accbd8b930bb1c2f45c6f7621861
febin123 <febsep96@gmail.com>
2022-12-14 18:31:27 +0000

    created create student form

--- front-end/studentManagementSystem/src/app/app.module.ts
+++ front-end/studentManagementSystem/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentListComponent } from './student-list/student-list.component';
 import { CreateStudentComponent } from './create-student/create-student.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +15,8 @@ import { CreateStudentComponent } from './create-student/create-student.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
