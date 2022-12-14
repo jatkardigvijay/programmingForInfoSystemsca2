4e97730ac7b82155ffac31779aa13de2e59f2ba2
febin123 <febsep96@gmail.com>
2022-12-14 16:03:07 +0000

    added new component of create student

--- front-end/studentManagementSystem/src/app/app-routing.module.ts
+++ front-end/studentManagementSystem/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateStudentComponent } from './create-student/create-student.component';
 import { StudentListComponent } from './student-list/student-list.component';
 
 const routes: Routes = [
   {path:'students',component:StudentListComponent},
+  {path:'create-student',component:CreateStudentComponent},
   {path:'',redirectTo:'students',pathMatch:'full'}
 ];
 
